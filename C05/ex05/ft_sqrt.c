4b3e984909f555c3742dfa0ba1f8bc5ece3eed51
oscarmorenosancho <63343472+oscarmorenosancho@users.noreply.github.com>
2022-08-31 13:37:43 +0200

    Correcion ex05

--- C05/ex05/ft_sqrt.c
+++ C05/ex05/ft_sqrt.c
@@ -6,20 +6,25 @@
 /*   By: omoreno- <omoreno-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 09:15:16 by omoreno-          #+#    #+#             */
-/*   Updated: 2022/08/31 09:15:36 by omoreno-         ###   ########.fr       */
+/*   Updated: 2022/08/31 13:28:41 by omoreno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_sqrt(int nb)
 {
 	int	i;
+	int	sq;
 
 	if (nb < 1)
 		return (0);
 	i = 1;
-	while ((i * i) < nb)
+	sq = 1;
+	while (sq < nb && sq > 0)
+	{
 		i++;
-	if ((i * i) == nb)
+		sq = i * i;
+	}
+	if (sq  == nb)
 		return (i);
 	return (0);
 }
--- C05/ex08/ft_ten_queens_puzzle.c
+++ C05/ex08/ft_ten_queens_puzzle.c
@@ -6,29 +6,11 @@
 /*   By: omoreno- <omoreno-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 09:23:23 by omoreno-          #+#    #+#             */
-/*   Updated: 2022/08/31 09:23:36 by omoreno-         ###   ########.fr       */
+/*   Updated: 2022/08/31 11:03:59 by omoreno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_ten_queens_puzzle(void)
 {
-    // 1) Start in the leftmost column
-    // 2) If all queens are placed
-    //     return true
-    // 3) Try all rows in the current column.  Do following
-    //      for every tried row.
-    //     a) If the queen can be placed safely in this row
-    //     then mark this [row, column] as part of the 
-    //     solution and recursively check if placing  
-    //     queen here leads to a solution.
-    //     b) If placing queen in [row, column] leads to a
-    //     solution then return true.
-    //     c) If placing queen doesn't lead to a solution 
-    //     then unmark this [row, column] (Backtrack) 
-    //     and go to step (a) to try other rows.
-    // 3) If all rows have been tried and nothing worked, 
-    //      return false to trigger backtracking.
-
-    //https://www.geeksforgeeks.org/printing-solutions-n-queen-problem/
-    return (0);
+	return (0);
 }
